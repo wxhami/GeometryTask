74b7aa80bafffcbb46850b88354c1a801a81a581
wxhami <d18102002@mail.ru>
2023-07-31 18:05:31 +0300

    add class Circle

---
+++ src/GeometryTask/GeometryTask/Circle.cs
@@ -0,0 +1,12 @@
+﻿namespace GeometryTask;
+
+public class Circle : FigureBase
+{
+    public override double CalculatorFigureleArea()
+    {
+        int radius = 0;
+        double area; 
+        area = 3.14 * (radius * 2);
+        return area;
+    }
+}
