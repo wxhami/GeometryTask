f151d51112f236c2b06ac76045990ccd6b0cecfd
wxhami <d18102002@mail.ru>
2023-07-31 18:20:23 +0300

    add class Triangle

---
+++ src/GeometryTask/GeometryTask/Triangle.cs
@@ -0,0 +1,15 @@
+﻿namespace GeometryTask;
+
+public class Triangle : FigureBase
+{
+    
+    public override double CalculatorFigureleArea()
+    {
+        double sideOne = 0;
+        double sideTwo = 0;
+        double sideThree = 0;
+        double area;
+        area = (sideOne + sideTwo + sideThree) / 2;
+        return area;
+    }
+}
