27e8ab18a669aec5e95c2649d778950c411a4988
wxhami <d18102002@mail.ru>
2023-08-01 13:21:41 +0300

    add ArraySort method

--- src/GeometryTask/GeometryTask/Triangle.cs
+++ src/GeometryTask/GeometryTask/Triangle.cs
@@ -2,14 +2,47 @@
 
 public class Triangle : FigureBase
 {
+    private double _sideOne;
+    private double _sideTwo;
+    private double _sideThree;
     
-    public override double CalculatorFigureleArea()
+    
+    public Triangle(double sideOne, double sideTwo, double sideThree)
     {
-        double sideOne = 0;
-        double sideTwo = 0;
-        double sideThree = 0;
+        this._sideOne = sideOne;
+        this._sideTwo = sideTwo;
+        this._sideThree = sideThree;
+    }
+
+    private bool PiphagorTheory()
+    {
+       
+
+        
+    }
+    
+    public override double CalculatorFigureArea()
+    {
         double area;
-        area = (sideOne + sideTwo + sideThree) / 2;
+        area = (_sideOne + _sideTwo + _sideThree) / 2;
         return area;
+    }
+
+    private void ArraySort()
+    {
+        double max;
+        double[] sides = new double[] { _sideOne, _sideTwo, _sideThree };
+        for (int i = 0; i < sides.Length - 1; i++)
+        {
+            for (int j = i + 1; j < sides.Length; j++)
+            {
+                if (sides[i].CompareTo(sides[j]) > 0)
+                {
+                    max = sides[j];
+                    sides[j] = sides[i];
+                    sides[i] = max;
+                }
+            }
+        }
     }
 }
