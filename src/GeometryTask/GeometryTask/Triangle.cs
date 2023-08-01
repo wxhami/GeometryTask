dd4000640693915e9b133ccce351d9d52e2789e3
wxhami <d18102002@mail.ru>
2023-08-01 14:36:13 +0300

    add CheckRectangular method

--- src/GeometryTask/GeometryTask/Triangle.cs
+++ src/GeometryTask/GeometryTask/Triangle.cs
@@ -5,44 +5,32 @@ public class Triangle : FigureBase
     private double _sideOne;
     private double _sideTwo;
     private double _sideThree;
-    
-    
+
     public Triangle(double sideOne, double sideTwo, double sideThree)
     {
-        this._sideOne = sideOne;
-        this._sideTwo = sideTwo;
-        this._sideThree = sideThree;
+        _sideOne = sideOne;
+        _sideTwo = sideTwo;
+        _sideThree = sideThree;
     }
 
-    private bool PiphagorTheory()
+    public bool CheckRectangular()
     {
-       
+        double[] array = {_sideOne, _sideTwo, _sideThree};
+        array.Sort();
+        double max = array[0];
+        double sumTwoSides = array[1] + array[2];
+        if (max != sumTwoSides)
+        {
+            return false;
+        }
 
-        
+        return true;
     }
-    
+
     public override double CalculatorFigureArea()
     {
         double area;
         area = (_sideOne + _sideTwo + _sideThree) / 2;
         return area;
-    }
-
-    private void ArraySort()
-    {
-        double max;
-        double[] sides = new double[] { _sideOne, _sideTwo, _sideThree };
-        for (int i = 0; i < sides.Length - 1; i++)
-        {
-            for (int j = i + 1; j < sides.Length; j++)
-            {
-                if (sides[i].CompareTo(sides[j]) > 0)
-                {
-                    max = sides[j];
-                    sides[j] = sides[i];
-                    sides[i] = max;
-                }
-            }
-        }
     }
 }
