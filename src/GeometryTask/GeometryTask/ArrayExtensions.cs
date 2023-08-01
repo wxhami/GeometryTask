7f01ba20365db662a61804ce700c7b34d0119c6f
wxhami <d18102002@mail.ru>
2023-08-01 14:09:54 +0300

    add ArrayExtensions class

---
+++ src/GeometryTask/GeometryTask/ArrayExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace GeometryTask;
+
+public static class ArrayExtensions
+{
+    public static void Sort(this double[] sides)
+    {
+        double max = double.MinValue;
+        for (int i = 0; i < sides.Length - 1; i++)
+        {
+            for (int j = i + 1; j < sides.Length; j++)
+            {
+                if (sides[i].CompareTo(sides[j]) > 0)
+                {
+                    max = sides[j];
+                    sides[j] = sides[i];
+                    sides[i] = max;
+                }
+            }
+        }
+    }
+}
