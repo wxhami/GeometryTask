e852a9a2d5e0b2ebda402f97138255dd0b8b6ff1
wxhami <d18102002@mail.ru>
2023-08-09 23:30:37 +0300

    ref constructor Triangle

--- src/GeometryTask/GeometryTask/Triangle.cs
+++ src/GeometryTask/GeometryTask/Triangle.cs
@@ -5,12 +5,22 @@ public class Triangle : FigureBase
     private double _sideOne;
     private double _sideThree;
     private double _sideTwo;
+    private double _sumOne;
+    private double _sumTwo;
+
 
     public Triangle(double sideOne, double sideTwo, double sideThree)
     {
-        _sideOne = sideOne;
-        _sideTwo = sideTwo;
-        _sideThree = sideThree;
+        if (_sideOne + _sideThree > _sideTwo || _sideThree + _sideTwo > _sideOne || _sideOne + _sumTwo > _sideThree)
+        {
+            _sideOne = sideOne;
+            _sideTwo = sideTwo;
+            _sideThree = sideThree;
+        }
+        else
+        {
+            throw new ArgumentException("incorrect sides of the triangle");
+        }
     }
 
     public bool CheckRectangular()
