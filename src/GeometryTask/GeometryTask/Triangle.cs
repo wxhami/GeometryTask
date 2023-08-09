f30fe7b18ee34d0712e06f743235621acf90e316
wxhami <d18102002@mail.ru>
2023-08-09 23:31:37 +0300

    ref constructor Triangle

--- src/GeometryTask/GeometryTask/Triangle.cs
+++ src/GeometryTask/GeometryTask/Triangle.cs
@@ -5,13 +5,11 @@ public class Triangle : FigureBase
     private double _sideOne;
     private double _sideThree;
     private double _sideTwo;
-    private double _sumOne;
-    private double _sumTwo;
 
 
     public Triangle(double sideOne, double sideTwo, double sideThree)
     {
-        if (_sideOne + _sideThree > _sideTwo || _sideThree + _sideTwo > _sideOne || _sideOne + _sumTwo > _sideThree)
+        if (_sideOne + _sideThree > _sideTwo || _sideThree + _sideTwo > _sideOne || _sideOne + _sideTwo > _sideThree)
         {
             _sideOne = sideOne;
             _sideTwo = sideTwo;
