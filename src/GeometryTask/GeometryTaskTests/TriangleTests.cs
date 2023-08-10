9a86d32665bbc02502be79fb1960ebafdca4d3fd
wxhami <d18102002@mail.ru>
2023-08-10 13:31:56 +0300

    refact constructor Circle

---
+++ src/GeometryTask/GeometryTaskTests/TriangleTests.cs
@@ -0,0 +1,20 @@
+﻿using GeometryTask;
+using Xunit;
+
+namespace GeometryTaskTests;
+
+public class TriangleTests
+{
+    [Theory()]
+    [InlineData(1, 3, 0)]
+    [InlineData(1, 2, 1)]
+    public void Circle_ShouldThrowArgumentException_WhenSidesIncorrect(double sideOne, double sideTwo, double sideThree)
+    {
+        //act
+        var exception = Assert.Throws<ArgumentException>(() => new Triangle(sideOne, sideTwo, sideThree));
+
+
+        //assert
+        Assert.Equal("incorrect sides of the triangle", exception.Message);
+    }
+}
